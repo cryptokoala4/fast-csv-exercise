9af9d0a75e0195cb42c768b4ec8e69285ca9d67b
Jacky Lee <jackywhlee@gmail.com>
2016-01-25 12:58:27 +0800

    moved stream validations up, added pesudo code for policies flaw

--- app.js
+++ app.js
@@ -18,35 +18,37 @@ var policies         = {};
 
 // start streaming data from customers.csv
 csv.fromStream(streamCustomers, {headers : ["id", "firstName", "lastName", "dob"]})
+  .validate(function(data){
+    // If validation fail, return false. If validation pass, return true
+    // Check to see if any column is missing
+    if (!!data.id && !!data.firstName && !!data.lastName && !!data.dob) {
+      return true;
+    } else {
+      customersError = true;
+      return false;
+    }
+    // later. check if there is duplicated ids
+  })
+  .on("data-invalid", function(data){
+    console.log("-----Customer Row Has An Error-----");
+    console.log(data);
+  })
   .on("data", function(data){
     customers[data.id] = {
       fullName: data.firstName + ' ' + data.lastName,
       dob: data.dob
     };
   })
-  .validate(function(data){
-    // If validation fail, return false. If validation pass, return true
-    // Check to see if any column is missing
-    if (!!data.id || !!data.firstName || !!data.lastName || !!data.dob) {
-      return true;
-    } else {
-      customersError = true;
-      return false;
-    }
-  })
-  .on("data-invalid", function(data){
-    console.log("-----Customer Row Has An Error-----");
-    console.log(data);
-  })
   .on("end", function(){
     // because streaming is asynchronous, we don't know which stream will finish last
     // therefore we have two boolean variable representing each stream
     // and generateReport only when both stream is completed
+    console.log(customers)
     if (customersError) {
       console.log("Please fix errors in customers.csv in order to proceed");
     } else {
-    customersDone = true;
-    generateReport();
+      customersDone = true;
+      generateReport();
     }
   });
 
@@ -55,18 +57,25 @@ csv.fromStream(streamPolicies  , {headers : ["policyNumber", "id", "insuranceTyp
   .validate(function(data){
     // If validation fail, return false. If validation pass, return true
     // Check to see if any column is missing
-    if (!!data.policyNumber || !!data.id || !!data.insuranceType) {
+    if (!!data.policyNumber && !!data.id && !!data.insuranceType) {
       return true;
     } else {
       policiesError = true;
       return false;
     }
+    // check if customers have both types or many more insurances
   })
   .on("data-invalid", function(data){
     console.log("-----Policy Row Has An Error-----");
     console.log(data);
   })
   .on("data", function(data){
+    // need to update functions to this format to fix policy overwrite problem
+    // if (policies[data.id] === undefined) {policies[data.id] = [];}
+    // policies[data.id].push({
+    //   policyNumber: data.policyNumber,
+    //   insuranceType: data.insuranceType
+    // })
     policies[data.id] = {
       policyNumber: data.policyNumber,
       insuranceType: data.insuranceType
@@ -76,6 +85,7 @@ csv.fromStream(streamPolicies  , {headers : ["policyNumber", "id", "insuranceTyp
     // because streaming is asynchronous, we don't know which stream will finish last
     // therefore we have two boolean variable representing each stream
     // and generateReport only when both stream is completed
+    console.log(policies)
     if (policiesError) {
       console.log("Please fix errors in polices.csv in order to proceed");
     } else {
@@ -123,6 +133,7 @@ function generateReport () {
 function validateReport (reports) {
   // test if any field in reports is missing
   // test if policy number contain 5 characters
+  // test if policy count matches report count
 
   // for each test, console.log whether the test pass or fail, the reason, and for which customer
   // return true when all validation checks out otherwise return false
